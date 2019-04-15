98d6205c575f1206c3f94ab0416347d15e662e88
kash <k.shaviakova@sam-solutions.com>
2019-04-15 17:58:17 +0300

    call simple jna library

--- backend/src/main/java/backend/demo/jna/api/JNAApiInterface.java
+++ backend/src/main/java/backend/demo/jna/api/JNAApiInterface.java
@@ -5,6 +5,6 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 
 public interface JNAApiInterface extends Library {
-    JNAApiInterface INSTANCE = (JNAApiInterface) Native.load("dll/Test", JNAApiInterface.class);
-    int sum(int x, int y);
+    JNAApiInterface INSTANCE = (JNAApiInterface) Native.load("dll/MathLibrary", JNAApiInterface.class);
+    int sum(int a, int b);
 }
